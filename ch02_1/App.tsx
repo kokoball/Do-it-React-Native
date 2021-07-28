551f02e738d9dc28aba3f090297c470a99af9eb9
unknown <kbp4154@naver.com>
2021-07-28 15:16:19 +0900

    first commit

---
+++ ch02_1/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function App() {
+  // console.log('App called')
+  const textElement = React.createElement(Text, null, 'Hello world!')
+  return textElement
+}
+
