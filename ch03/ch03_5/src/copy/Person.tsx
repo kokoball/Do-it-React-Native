73a932c2154378dade48d3adf1cf02fd5d469334
unknown <kbp4154@naver.com>
2021-08-02 15:43:36 +0900

    8.2 ch03 finish

--- ch03/ch03_5/src/copy/Person.tsx
+++ ch03/ch03_5/src/copy/Person.tsx
@@ -1,5 +1,3 @@
-// 컴파일 오류만 나지 않게 구현한 초기 모습
-/*
 import React, {FC} from 'react'
 import {Text} from 'react-native'
 import * as D from '../data'
@@ -12,7 +10,7 @@ const Person: FC<PersonProps> = ({person}) => {
   return <Text>{JSON.stringify(person, null, 2)}</Text>
 }
 export default Person
-*/
+
 // Person 컴포넌트 초기 스타일
 /*
 import React from 'react'
@@ -53,88 +51,88 @@ const Person: FC<PersonProps> = ({person}) => {
 export default Person
 */
 // 최종 모습
-import React from 'react'
-import type {FC} from 'react'
-import {View, Text, Image, Alert} from 'react-native'
-import {Colors} from 'react-native-paper'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
-import moment from 'moment-with-locales-es6'
-import * as D from '../data'
-import {Avatar, IconText} from '../components'
-import {styles} from './Person.style'
+// import React from 'react'
+// import type {FC} from 'react'
+// import {View, Text, Image, Alert} from 'react-native'
+// import {Colors} from 'react-native-paper'
+// import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+// import moment from 'moment-with-locales-es6'
+// import * as D from '../data'
+// import {Avatar, IconText} from '../components'
+// import {styles} from './Person.style'
 
-moment.locale('ko')
+// moment.locale('ko')
 
-export type PersonProps = {
-  person: D.IPerson
-}
-const avatarPressed = () => Alert.alert('avatar pressed.')
-const deletePressed = () => Alert.alert('delete pressed.')
-const countIconPressed = (name: string) => () => Alert.alert(`${name} pressed.`)
+// export type PersonProps = {
+//   person: D.IPerson
+// }
+// const avatarPressed = () => Alert.alert('avatar pressed.')
+// const deletePressed = () => Alert.alert('delete pressed.')
+// const countIconPressed = (name: string) => () => Alert.alert(`${name} pressed.`)
 
-const Person: FC<PersonProps> = ({person}) => {
-  return (
-    <View style={[styles.view]}>
-      <View style={[styles.leftView]}>
-        <Avatar
-          imageStyle={[styles.avatar]}
-          uri={person.avatar}
-          size={50}
-          onPress={avatarPressed}
-        />
-      </View>
-      <View style={[styles.rightView]}>
-        <Text style={[styles.name]}>{person.name}</Text>
-        <Text style={[styles.email]}>{person.email}</Text>
-        <View style={[styles.dateView]}>
-          <Text style={[styles.text]}>
-            {moment(person.createdDate).startOf('day').fromNow()}
-          </Text>
-          <Icon
-            name="trash-can-outline"
-            size={26}
-            color={Colors.lightBlue500}
-            onPress={deletePressed}
-          />
-        </View>
-        <Text
-          numberOfLines={3}
-          ellipsizeMode="tail"
-          style={[styles.text, styles.comments]}>
-          {person.comments}
-        </Text>
-        <Image style={[styles.image]} source={{uri: person.image}} />
-        <View style={[styles.countsView]}>
-          <IconText
-            viewStyle={[styles.touchableIcon]}
-            onPress={countIconPressed('comment')}
-            name="comment"
-            size={24}
-            color={Colors.blue500}
-            textStyle={[styles.iconText]}
-            text={person.counts.comment}
-          />
-          <IconText
-            viewStyle={[styles.touchableIcon]}
-            onPress={countIconPressed('retweet')}
-            name="twitter-retweet"
-            size={24}
-            color={Colors.purple500}
-            textStyle={[styles.iconText]}
-            text={person.counts.retweet}
-          />
-          <IconText
-            viewStyle={styles.touchableIcon}
-            onPress={countIconPressed('heart')}
-            name="heart"
-            size={24}
-            color={Colors.red500}
-            textStyle={[styles.iconText]}
-            text={person.counts.heart}
-          />
-        </View>
-      </View>
-    </View>
-  )
-}
-export default Person
+// const Person: FC<PersonProps> = ({person}) => {
+//   return (
+//     <View style={[styles.view]}>
+//       <View style={[styles.leftView]}>
+//         <Avatar
+//           imageStyle={[styles.avatar]}
+//           uri={person.avatar}
+//           size={50}
+//           onPress={avatarPressed}
+//         />
+//       </View>
+//       <View style={[styles.rightView]}>
+//         <Text style={[styles.name]}>{person.name}</Text>
+//         <Text style={[styles.email]}>{person.email}</Text>
+//         <View style={[styles.dateView]}>
+//           <Text style={[styles.text]}>
+//             {moment(person.createdDate).startOf('day').fromNow()}
+//           </Text>
+//           <Icon
+//             name="trash-can-outline"
+//             size={26}
+//             color={Colors.lightBlue500}
+//             onPress={deletePressed}
+//           />
+//         </View>
+//         <Text
+//           numberOfLines={3}
+//           ellipsizeMode="tail"
+//           style={[styles.text, styles.comments]}>
+//           {person.comments}
+//         </Text>
+//         <Image style={[styles.image]} source={{uri: person.image}} />
+//         <View style={[styles.countsView]}>
+//           <IconText
+//             viewStyle={[styles.touchableIcon]}
+//             onPress={countIconPressed('comment')}
+//             name="comment"
+//             size={24}
+//             color={Colors.blue500}
+//             textStyle={[styles.iconText]}
+//             text={person.counts.comment}
+//           />
+//           <IconText
+//             viewStyle={[styles.touchableIcon]}
+//             onPress={countIconPressed('retweet')}
+//             name="twitter-retweet"
+//             size={24}
+//             color={Colors.purple500}
+//             textStyle={[styles.iconText]}
+//             text={person.counts.retweet}
+//           />
+//           <IconText
+//             viewStyle={styles.touchableIcon}
+//             onPress={countIconPressed('heart')}
+//             name="heart"
+//             size={24}
+//             color={Colors.red500}
+//             textStyle={[styles.iconText]}
+//             text={person.counts.heart}
+//           />
+//         </View>
+//       </View>
+//     </View>
+//   )
+// }
+// export default Person
--- ch03/ex/src/components/TouchableView.tsx
+++ ch03/ex/src/components/TouchableView.tsx
@@ -4,14 +4,13 @@ import {TouchableOpacity, View} from 'react-native'
 import type {StyleProp, ViewStyle} from 'react-native'
 
 type TouchableOpacityProps = ComponentProps<typeof TouchableOpacity>
+
 export type TouchableViewProps = TouchableOpacityProps & {
   viewStyle?: StyleProp<ViewStyle>
 }
-
+// prettier-ignore
 export const TouchableView: FC<TouchableViewProps> = ({
-  children,
-  viewStyle,
-  ...touchableProps
+  children, viewStyle, ...touchableProps
 }) => {
   return (
     <TouchableOpacity {...touchableProps}>
